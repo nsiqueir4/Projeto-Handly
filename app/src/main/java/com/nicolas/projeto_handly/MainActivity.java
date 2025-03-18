476f561a4d671aebf9a54349a1af0ac79368297f
Nicolas Siqueira <164067088+nsiqueir4@users.noreply.github.com>
2025-03-18 17:06:20 -0300

    att

--- app/src/main/java/com/nicolas/projeto_handly/MainActivity.java
+++ app/src/main/java/com/nicolas/projeto_handly/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         navegarTelaHome();
     }
     private void navegarTelaHome() {
-        Button btnEntrar = findViewById(R.id.btnEntrar);
+        ImageButton btnEntrar = findViewById(R.id.btnEntrar);
         btnEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
